c529af7f3b46edaf491ee40dd5f98a995af31a5f
HUGU-12 <shemahugu12@gmail.com>
2024-07-06 21:51:51 +0200

    changed the click listener from click to keydown and keyup

--- Javascript Drum Kit/script.js
+++ Javascript Drum Kit/script.js
@@ -1,24 +1,39 @@
-//create an object with keys corresponding to the sound files
+
 const keySounds = {
-  clap: 'clap.wav',
-  hihat: 'hihat.wav',
-  kick: 'kick.wav',
-  openhat: 'openhat.wav',
-  boom: 'boom.wav',
-  ride: 'ride.wav',
-  snare: 'snare.wav',
-  tom:'tom.wav',
-  tink: 'tink.wav'
-};
+  A: '/Javascript Drum Kit/sounds/clap.wav', 
+  S: '/Javascript Drum Kit/sounds/hihat.wav', 
+  D: '/Javascript Drum Kit/sounds/kick.wav', 
+  F: '/Javascript Drum Kit/sounds/openhat.wav', 
+  G: '/Javascript Drum Kit/sounds/boom.wav',
+  H: '/Javascript Drum Kit/sounds/ride.wav',
+  J: '/Javascript Drum Kit/sounds/snare.wav', 
+  K: '/Javascript Drum Kit/sounds/tom.wav',
+  L: '/Javascript Drum Kit/sounds/tink.wav'
+}
 
-const soundNames = Object.keys(keySounds);
-
-const keys = document.querySelectorAll('.key');//this yields a node list which is iterable
-
-keys.forEach((key,index) =>{
-  key.addEventListener('click',() =>{
-    const sound = new Audio(`sounds/${keySounds[soundNames[index]]}`);
-    sound.play();
+  function handleKeyPress(event, isKeyDown) {
+    const pressedKey = event.key.toUpperCase();
+    const soundFile = keySounds[pressedKey];
+    const keyElement = document.querySelector(`[data-key="${pressedKey}"]`);
+  
+    if (keyElement) {
+      if (isKeyDown) {
+        const audio = new Audio(soundFile);
+        audio.play();
+        keyElement.classList.add('pressed');
+      } else {
+        keyElement.classList.remove('pressed');
+      }
+    }
+  }
+  
+  document.addEventListener('keydown', (event) => {
+    handleKeyPress(event, true);
   });
-});
-console.log(soundNames);
+  
+  document.addEventListener('keyup', (event) => {
+    handleKeyPress(event, false);
+  });
+  
+
+
